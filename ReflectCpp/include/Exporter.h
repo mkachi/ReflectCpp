58d1d8ed6dbf780d88821bd19e5d93c45d38445d
MKachi <mkachi@naver.com>
2020-02-12 14:04:01 +0900

    프로젝트 추가

---
+++ ReflectCpp/include/Exporter.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifdef _MSC_VER
+	#ifdef DLL_EXPORTS
+		#define DLL_API __declspec(dllexport)
+	#else
+		#define DLL_API __declspec(dllimport)
+	#endif
+#else
+	#define DLL_API
+#endif
+
+#if defined(DLL_API)
+	#define EXPORT_API DLL_API
+#endif
